fdd786938fc710df44291c7095ff679e25dc0e8d
Maro <grafika.marekduda@gmail.com>
2017-06-04 19:19:47 +0200

    added new variables

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,13 @@
 function getTriangleArea(a, h) {
-  if ( a, h > 0 ) {
-    console.log('Prawidłowe dane');
-  } else if ( a, h <= 0 ) {
-    console.log('Nieprawidłowe dane');
-  }
-    
+  if (a > 0 && h > 0) {
+  }  
   return a * h / 2
 }
 
-console.log( getTriangleArea(10,6) );
+console.log(getTriangleArea(10, 6));
+
+var triangle1Area = getTriangleArea(5, 5);
+var triangle2Area = getTriangleArea(7, 8);
+var triangle3Area = getTriangleArea(12, 14);
+
+console.log(triangle1Area, triangle2Area, triangle3Area);
